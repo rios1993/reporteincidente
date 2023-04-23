6a6845933a54ac1bf7a8ca2a10bcf66d035d5e31
Carlos <u201301136@upc.edu.pe>
2023-04-22 21:35:03 -0400

    Version CR5

--- app/src/main/java/com/upc/reporteincidente/MainActivity.java
+++ app/src/main/java/com/upc/reporteincidente/MainActivity.java
@@ -2,13 +2,30 @@ package com.upc.reporteincidente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView txtFullname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtFullname = findViewById(R.id.txtFullname);
+
+        Intent intent = getIntent();
+
+        String usuario = intent.getStringExtra("usuario");
+        String fullname = intent.getStringExtra("fullname");
+        String privilegio = intent.getStringExtra("privilegio");
+
+        txtFullname.setText(fullname);
     }
 }
